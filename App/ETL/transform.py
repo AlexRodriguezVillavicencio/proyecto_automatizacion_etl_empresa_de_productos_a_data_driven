96f4e16a9910770dba441cb88347bcff54f75f83
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 <arodriguezv@uni.pe>
2022-06-24 11:11:44 -0500

    proceso de transformación

---
+++ App/ETL/transform.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os 
+import numpy as np
+
+def transform_compra(df):
+    #eliminación de duplicados
+    df.drop_duplicates(inplace=True)
+    pass
+def transform_gasto():
+    pass
+def transform_localidades():
+    pass
+def transform_proveedores():
+    pass
+def transform_sucursales():
+    pass
+def transform_venta():
+    pass
+def transform_clientes():
+    pass
+def transform_canal_venta():
+    pass
+def transform_tipo_gasto():
+    pass
