4454f9f0bb5f0a364be56b5b75a987489199ce5c
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 <arodriguezv@uni.pe>
2022-06-24 11:09:50 -0500

    ignorando...

--- App/main.py
+++ App/main.py
@@ -3,10 +3,13 @@ import os
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 
+from ETL.extract import carga_archivo
+
 # instancia del objeto Flask
 app = Flask(__name__)
 # Carpeta de subida
-app.config['UPLOAD_FOLDER'] = '../Datasets'
+app.config['UPLOAD_FOLDER'] = "../Datasets"
+
 
 @app.route("/")
 def upload_file():
@@ -15,17 +18,22 @@ def upload_file():
 
 @app.route("/upload", methods=['POST'])
 def uploader():
- if request.method == 'POST':
-  # obtenemos el archivo del input "archivo"
-  f = request.files['archivo']
-  filename = secure_filename(f.filename)
-  # Guardamos el archivo en el directorio "Archivos PDF"
-  f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+  if request.method == 'POST':
+    # obtenemos el archivo del input "archivo"
+    f = request.files['archivo']
+    filename = secure_filename(f.filename)
+    # Guardamos el archivo en el directorio "Archivos PDF"
+    f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    
+    sep =request.form['sep']
+    encoding =request.form['encoding']
 
+    carga_archivo(filename, sep, encoding)
 
-  # Retornamos una respuesta satisfactoria
-  return "<h1>Archivo subido exitosamente</h1>"
+    # Retornamos una respuesta satisfactoria
+    return 'respuesta exitosa'
+
 
 if __name__ == '__main__':
- # Iniciamos la aplicación
- app.run(debug=True)
+  # Iniciamos la aplicación
+  app.run(debug=True)
