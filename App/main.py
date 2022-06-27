427bfce989ad859a1e77c6f89cdd12fee7ee7067
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 <arodriguezv@uni.pe>
2022-06-27 18:38:32 -0500

    main

--- App/main.py
+++ App/main.py
@@ -1,39 +1,42 @@
-# Importamos todo lo necesario
 import os
-from flask import Flask, render_template, request
-from werkzeug.utils import secure_filename
+from flask import Flask, render_template, jsonify, request
 
+from pathlib import Path
 from ETL.extract import carga_archivo
 
 # instancia del objeto Flask
 app = Flask(__name__)
-# Carpeta de subida
-app.config['UPLOAD_FOLDER'] = "../Datasets"
+
+#app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'database')
+CURRENT_DIRECTORY = os.getcwd() + "/database/"
 
 
 @app.route("/")
 def upload_file():
- # renderisamos la plantilla "formulario.html"
  return render_template('formulario.html')
 
 @app.route("/upload", methods=['POST'])
 def uploader():
-  if request.method == 'POST':
-    # obtenemos el archivo del input "archivo"
-    f = request.files['archivo']
-    filename = secure_filename(f.filename)
-    # Guardamos el archivo en el directorio "Archivos PDF"
-    f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    
-    sep =request.form['sep']
-    encoding =request.form['encoding']
+  try:
+    f = request.files.getlist("mi_base_de_datos")
+    for file in f:
+      directory_file = file.filename.split("/")
+      print(file)
+      print(file.filename)
+      file_d = directory_file.pop()
+      if os.path.exists(CURRENT_DIRECTORY) == False:
+        os.mkdir(path=CURRENT_DIRECTORY)
+        file.save(CURRENT_DIRECTORY + file_d)
+      else:
+        file.save(CURRENT_DIRECTORY + file_d)
+    return jsonify({"message":"ok"})
+  except FileNotFoundError:
+    return jsonify({"message":"FileNotFoundError"})
 
-    carga_archivo(filename, sep, encoding)
-
-    # Retornamos una respuesta satisfactoria
-    return 'respuesta exitosa'
-
+for ruta in list(Path('database').iterdir()):
+    ruta = os.path.join(os.getcwd(), ruta)
+    print(ruta)
+    carga_archivo(ruta)
 
 if __name__ == '__main__':
-  # Iniciamos la aplicación
   app.run(debug=True)
